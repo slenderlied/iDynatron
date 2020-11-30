5a5095ac78ac9f74e9fcdd96a315d40b3eb749ee
slenderlied <alejandro.ry98@gmail.com>
2020-11-30 20:20:18 -0300

    master

--- public/php/tcocalculator.php
+++ public/php/tcocalculator.php
@@ -1,90 +1,192 @@
+<!-- Cambiar esto :c -->
+<!-- :c -->
 <?php
-
 include ("../database/mongodb.php");
 session_start();
+$bulk = new MongoDB\Driver\BulkWrite;
 $usuario = $_SESSION["correo"];
-$sumstorage = "0";
-$fechafilter = $_GET["fecha"];
-$idrandom = $_GET["idrandom"];
-$i = 0;
-$filter = [
-    'Correo_usuario' => $usuario
-    ];
-    $query = new MongoDB\Driver\Query($filter);
 
-$query1 = new MongoDB\Driver\Query([]);
+$hardware = $_POST["nombrehardware"];
 
-$cursor2 = $manager->executeQuery($dbname4, $query1);
+$idrandom = rand(1, 999999);
+date_default_timezone_set("America/Santiago");
+$dt = new DateTime("now", new DateTimeZone('America/Santiago'));
+$fecha = Date("d-m-Y-H:i");
 
+$query = new MongoDB\Driver\Query([]);
 
-$cursor1 = $manager->executeQuery($dbname1, $query);
+// Costo Personal TI  \\
+$horasaño = "40";
+$personal = "23";
+$costopersonal = $horasaño * $personal;
+echo $costopersonal;
+// Horas al año -> 40 || Costo personal TI por hora -> 23 \\
 
-        // foreach($cursor1 as $row1){
-        //    $fechavalidator = $row1 -> Fecha_Validator;
-        //     $idvalidator = $row1 -> ID_Validator;
+// Hardware 
+$porc = "1.20";
+$cursor = $manager->executeQuery($dbname4, $query);
 
-        //    if($fechafilter == $fechavalidator && $idrandom == $idvalidator ){
-        //      $especificaciones = $row1 -> Especificaciones;
+foreach ($cursor as $row ) {
+    
+   $nombrehardware = $row -> nombrehardware;
+   
+   if ($hardware==$nombrehardware) {
+     $costohardware = $row -> selection2;
+    echo "<br>";
+   $costohardwaretotal = round($costohardware * $porc);
+   echo $costohardwaretotal;
+  
+}
+}
+//--\\
 
-        //     foreach ($cursor2 as $key[$i]) {
-        //         $nombrehardware = $key[$i] -> nombrehardware;
-        //         $costohardware = $key[$i] -> selection2;
-                
-        //        if ($fechafilter == $fechavalidator && $idrandom == $idvalidator && $nombrehardware==$especificaciones) {
-        //         $storageprocesador = $row1 -> Procesador;
+// Software
+$software = substr($hardware,13,-35);
+$software;
 
-        //         //  $especificaciones = $row1 -> Especificaciones;
-        //          $resultado = substr( $especificaciones,35 );
-        //          echo $resultado;
-                
-                 
-        //        }
+$cursor1 = $manager->executeQuery($dbname5, $query);
+ 
+foreach ($cursor1 as $soft ) {
+      $nucleosoftware = $soft -> nucleo;
+    if ($software == $nucleosoftware) {
+      echo "<br>";
+      $valornucleo = $soft -> costonucloe;
+     echo $costosoftaretotal = $valornucleo;
+}
+}
+//--\\
 
-        //     }
+// Electricidad 
+$cursor2 = $manager->executeQuery($dbname3, $query);
+$precioelectricidad = "0.133";
 
-        // }else{
+foreach ($cursor2 as $electri ) {
+    
+   $hardwareconsumo = $electri -> hardwareconsumo;
+   
+   if ($hardware==$hardwareconsumo) {
+   echo "<br>";
+   $consumowatt = $electri -> consumowatt;
+   $consumototalwatt = $precioelectricidad + $consumowatt;
+   echo $consumototalwatt;
+}
+}
+//--\\
 
-        // }
+// Redes
+echo "<br>";
+echo $costototalhardsoft = $costohardwaretotal + $costosoftaretotal;
+echo "<br>";
+$porcredes = "1.15";
+echo $totalredes = round($costototalhardsoft * $porcredes);
+echo "<br>";
+//--\\
 
-        // }
+//Mantención redes
+echo $manteredes = round($totalredes*$porcredes);
+//--\\
 
+// Suma total \\
+echo "<br>";
+echo $sumatotalfinal = round($costopersonal + $costohardwaretotal + $costosoftaretotal + $consumototalwatt + $totalredes + $manteredes);
+echo "<br>";
+echo $sumatotalfinalaños = $sumatotalfinal * 5;
+//--\\
+echo "<br>";
 
+// Cloud \\
+$cursor3 = $manager->executeQuery($dbname6, $query);
+$maquinarandom = rand(1, 8);
+$horasmes = "730";
+$i= 0;
 
+foreach ($cursor3 as $maquina ) {
+    
+    if ($maquinarandom == $i) {
+      $nombremaquina = $maquina -> nombrevirtualmachine;
+     echo "<br>";
+    $costoporhora = str_replace(',','.',$maquina -> usoporhora); 
+    }
+    $i = $i + 1;
+}
 
+echo $cloudproducto = round($horasmes * $costoporhora);
+echo "<br>";
+//--\\
 
+// Costo Personal Cloud  \\
+$horasañocloud = "16.67";
+$personalcloud = "23";
+$costopersonalcloud = round($horasañocloud * $personalcloud);
+echo $costopersonalcloud;
+//--\\
 
+// Costo Almacenamiento \\
+// Costo Storage \\
+$cursor4 = $manager->executeQuery($dbname7, $query);
+$standard = "Standard Storage";
+$volumegb = "512";
+foreach ($cursor4 as $stora) {
+    $nombrest = $stora -> nombrestorage;
+    $nombrestora = substr($nombrest,0, 16);
+    if ($standard == $nombrestora) {
+        $preciostorage = $stora -> preciostorage;
+    }
+}
+echo "<br>";
+echo $precioalmacenamiento= round($volumegb * $preciostorage);
+//--\\
 
-$cursor = $manager->executeQuery($dbname2, $query);
+// Costo Red \\
+$cursor5 = $manager->executeQuery($dbname8, $query);
+$usomensualstandard = "0-1";
+$redusodigitada = "1024";
+foreach ($cursor5 as $red) {
+    $usomensual = $red -> usoMensual;
+    if ($usomensualstandard == $usomensual) {
+        $preciored =  str_replace(',','.',$red -> precioRed);
+       echo "<br>";
+       echo $precioredalmacenamiento = round(($precioalmacenamiento)+($redusodigitada*$preciored));
+    }
+}
 
-        foreach($cursor as $row){
-           $fechavalidator = $row -> Fecha_Validator;
-        //    echo $fechavalidator;
+//Suma Total Cloud\\
+$sumatotalcloud = $cloudproducto + $costopersonalcloud + $precioalmacenamiento + $precioredalmacenamiento;
+$sumatotalcloudaños = $sumatotalcloud * 5;
 
-           $storage = $row -> Storage;
-        //    echo $storage;
+//--\\
 
-           $storageprocesador = $row -> Procesador;
-        //    echo $storageprocesador;
+    try {
 
-           $cantstorage = $row -> CantStorage;
-           $sumstorage = $sumstorage + $cantstorage;
-        //    echo $sumstorage;
+        $tcocalculos= [
+            '_id' => new MongoDB\BSON\ObjectID,
+            'Correo_Usuario' => $usuario,
+            'Fecha_Validator' => $fecha,
+            'Id_Validator' => $idrandom,
+            'Costo_Personal_TI_Premise' => $costopersonal,
+            'Hardware_Premise' => $costohardwaretotal,
+            'Software_Premise' => $costosoftaretotal,
+            'Electricidad_Premise' => $consumototalwatt,
+            'Redes_Premise' => $totalredes,
+            'Mantencion_Redes_Premise' => $manteredes,
+            'Suma_Total_Premise' => $sumatotalfinal,
+            'Suma_Total_Premise_5' => $sumatotalfinalaños,
+            'Producto_Cloud' => $cloudproducto,
+            'Costo_Personal_Cloud' => $costopersonalcloud,
+            'Costo_Almacenamiento_Cloud' => $precioalmacenamiento,
+            'Costo_Red_Cloud' => $precioredalmacenamiento,
+            'Suma_Total_Cloud' => $sumatotalcloud,
+            'Suma_Total_Cloud_5' => $sumatotalcloudaños
+            ];
 
+            $bulk->insert($tcocalculos);
+    
+            $result = $manager->executeBulkWrite($dbname10, $bulk);
 
-           if($fechafilter == $fechavalidator){
-
-            // echo "1";
-            // echo "<br>";
-        }else{
-
-                // echo "2";
-                // echo "<br>";
-        }
-
-        }
-
-    // header("Location: /resultados?fecha=$fechafilter&idrandom=$idrandom");
-     
-                
-        
+          $bulk->insert($storage);              
+          header("Location: /resultados?fecha=$fecha&idrandom=$idrandom");
+   } catch (MongoDB\Driver\Exception\Exception $e) {
+       die("Error Encontrado: ".$e);
+   }
+//-------------------------------------------------\\
 ?>
--- resources/views/resultados.blade.php
+++ resources/views/resultados.blade.php
@@ -68,77 +68,23 @@ $filter = [
     </header>
 
 
-   
-    <canvas id="myChart" style="height:15vh; width:30vw;"></canvas>
+    <section class="ftco-section contact-section" id="contact">
+		<div class="container">
+			<div class="row block-15">
 
+				<div class="col-md-6 ftco-animate">
+        <canvas id="myChart" style="height:15vh; width:30vw;"></canvas>
+				</div>
 
+				<div class="col-md-6 d-flex align-items-stretch">
+					<div id="map">     
+          <canvas id="myChart2" style="height:15vh; width:30vw;"></canvas>
+          </div>
+				</div>
+			</div>
 
-    <main>
-
- 
-      <table>
-        <thead>
-          <tr>
-            <th class="service">SERVICE</th>
-            <th class="desc">DESCRIPTION</th>
-            <th>PRICE</th>
-            <th>TOTAL GASTOS MES</th>
-            <th>TOTAL GASTOS 5 AÑOS</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td class="service">Design</td>
-            <td class="desc">Creating a recognizable design solution based on the company's existing visual identity</td>
-            <td class="unit">$40.00</td>
-            <td class="qty">26</td>
-            <td class="total">$1,040.00</td>
-          </tr>
-          <tr>
-            <td class="service">Development</td>
-            <td class="desc">Developing a Content Management System-based Website</td>
-            <td class="unit">$40.00</td>
-            <td class="qty">80</td>
-            <td class="total">$3,200.00</td>
-          </tr>
-          <tr>
-            <td class="service">Personal TI</td>
-            <td class="desc">Initial training sessions for staff responsible for uploading web content</td>
-            <td class="unit">$<?php echo $costopersonalti?></td>
-            <td class="qty">4</td>
-            <td class="total">$160.00</td>
-          </tr>
-          <tr>
-            <td colspan="4">SUBTOTAL</td>
-            <td class="total">$5,200.00</td>
-          </tr>
-          <tr>
-            <td colspan="4">TAX 25%</td>
-            <td class="total">$1,300.00</td>
-          </tr>
-          <tr>
-            <td colspan="4" class="grand total">GRAND TOTAL</td>
-            <td class="grand total">$6,500.00</td>
-          </tr>
-        </tbody>
-      </table>
-      <div id="notices">
-        <div>Informe:</div>
-        <div class="notice">
-          <form action="/pdf" method="get">   
-          <?php echo "<button type='input' class='btn btn-outline-dark'>Ver Informe PDF</button>";?> 
-          <input type="hidden" name="fecha" value="<?php echo $fechafilter ?>">
-          </form> 
-        </div>
-      </div>
-    </main>
-
-
-    <!-- <footer>
-      Invoice was created on a computer and is valid without the signature and seal.
-    </footer> -->
-
-
+		</div>
+	</section>
 
     <script>
 var ctx = document.getElementById('myChart').getContext('2d');
@@ -185,7 +131,50 @@ var myPieChart = new Chart(ctx, {
 
 </script>
 
+<script>
+var ctx = document.getElementById('myChart2').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Primer Año', 'Segundo Año', 'Tercer Año', 'Cuarto Año', 'Quinto Año'],
+        datasets: [{
+            label: 'Gasto On-Premise',
+            data: [ <?php echo $sumtotmes ?>, <?php echo $sumtotmes*2 ?>, <?php echo $sumtotmes*3 ?>, <?php echo $sumtotmes*4 ?>,<?php echo $sumtotmes*5 ?>],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.8)',
+                'rgba(54, 162, 235, 0.8)',
+                'rgba(255, 206, 86, 0.8)',
+                'rgba(75, 192, 192, 0.8)',
+                'rgba(153, 102, 255, 0.8)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)'
+            ],
+            borderWidth: 5
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
 
+var myPieChart = new Chart(ctx, {
+    type: 'pie',
+    data: data,
+    options: options
+});
+
+</script>
 
 
 
